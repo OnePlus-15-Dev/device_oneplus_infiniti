4bf0c35d6ba858d7ec4dce927375c0eb326fecb2
chandu078 <chandudyavanapelli03@gmail.com>
2026-02-06 12:16:02 +0530

    infiniti: Initial device tree conversion
    Change-Id: Ibed6a23f128fdc81f157a58609a2eefe24a8321e

--- extract-files.py
+++ extract-files.py
@@ -19,8 +19,8 @@ from extract_utils.main import (
 
 namespace_imports = [
     'hardware/oplus',
-    'hardware/qcom-caf/sm8650',
-    'vendor/oneplus/sm8650-common',
+    'hardware/qcom-caf/sm8850',
+    'vendor/oneplus/sm8850-common',
     'vendor/qcom/opensource/commonsys-intf/display',
 ]
 
@@ -91,7 +91,7 @@ blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
 
 module = ExtractUtilsModule(
-    'waffle',
+    'infiniti',
     'oneplus',
     namespace_imports=namespace_imports,
     blob_fixups=blob_fixups,
@@ -101,6 +101,6 @@ module = ExtractUtilsModule(
 
 if __name__ == '__main__':
     utils = ExtractUtils.device_with_common(
-        module, 'sm8650-common', module.vendor
+        module, 'sm8850-common', module.vendor
     )
     utils.run()
